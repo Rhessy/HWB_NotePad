c205e57438df85acb32b0126b4729511c7e11637
Riley Hess <RileyH@beckhoff.com>
2024-11-21 13:02:29 -0600

    adding in some functionality to add in p elements under the side class element everytime a new line character is added to the text area

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ let hamburgerLine = document.querySelectorAll('.hamburgerLine');
 let side = document.querySelector('.side');
 let codeBlock = document.querySelector('.codeBlock');
 let codeLine = document.querySelector('.codeLine');
+let charCount = document.querySelector('.characterCount');
+let LinCount = document.querySelector('.lineCount');
+let pElements = [];
+
 
 
 
@@ -49,32 +53,144 @@ copy.addEventListener('click', ()=>{
     alert("Copied the text: " + codeSnip.value);
 })
 
-codeSnip.addEventListener('input', ()=>{
+
+/* Adding in code for text area */
+
+let charactersPerLine = 10;
+let maxLines = 6;
+let widthPerCharacter = 10;
+let numOfRow;
+
+function formatTextAsRequired(){
+    //get the current value of codeSnip
+    let textInput = document.querySelector('.codeSnip').value;
+
+    //create rows using the new line string
+    let inputAsRows = textInput.split("\n");
+
+    //removes all line breaks 
+    let inputAsOneLine = textInput.replace(/(\r\n\t|\n|\r\t)/gm,"");
+
+    //read current cursor position 
+    cursorPositionOnInput=document.querySelector(".codeSnip").selectionStart;
+
+    //set default value for cursor offset. cursor offset is needed when re-posiotioning the cursor after WRITE DATA
+    var cursorOffsetAfterOutput=0;
+
+//number of visible characters per line before text breaks without a line-break. 
+//Depends on width of textarea and width of characters entered.
+    var visibleCharactersPerLine=Math.floor((document.querySelector(".codeSnip").offsetWidth)/9);
+    
+
+
+
+
+  let additionalTextAreaRows=0; //additional rows needed due to the text breaking
+  let totalRows=inputAsRows.length; //don't put inputAsRows.length in the for statement, as the array is growing in the loop which results in an infinite loop
+  let row;
+  let lineBreakCount=0;
+  let characterCount=0;
+  for (row = 0; row < totalRows; ++row) {
+    if(inputAsRows[row].length>charactersPerLine){ //1b DETECT IF TEXT PER LINE IS TOO LONG 
+      if (inputAsRows[row+1] === undefined) {
+        inputAsRows[row+1]="";// the row did not exist
+        totalRows++;
+        }
+      //1c PUSH OVERFLOWING TEXT: move text that is too long for this row to the next row:
+      inputAsRows[row+1]=inputAsRows[row].substring(charactersPerLine)+inputAsRows[row+1];
+      inputAsRows[row]=inputAsRows[row].substring(0,charactersPerLine);
+      //determine, if cursor was at the end of the line that got a line-break:
+      var newOutput=inputAsRows.join("\n");
+      if(newOutput.substr(cursorPositionOnInput-1,1)=="\n"){
+        cursorOffsetAfterOutput=1; }
+      }
+    if(inputAsRows[row].length>visibleCharactersPerLine){ //1b DETECT IF TEXT PER LINE IS TOO LONG 
+      additionalTextAreaRows=additionalTextAreaRows+Math.floor(inputAsRows[row].length/visibleCharactersPerLine);
+      }
+    
+    }
+
+  if(inputAsRows.length<=maxLines && inputAsOneLine.length<=(maxLines*charactersPerLine)){//data is within max number of rows and max total digits
+    textOutput=inputAsRows.join("\n");
+    document.querySelector(".codeSnip").rows=inputAsRows.length+additionalTextAreaRows;//resize textarea    
+    //document.getElementById("errors").innerHTML="";//remove error message
+    //document.getElementById("count").innerHTML=inputAsOneLine.length+"/"+(maxLines*charactersPerLine);//show digits count
+   /* if(onPaste){
+      cursorOffsetAfterOutput=cursorOffsetOnPaste(textInput,cursorPositionOnInput,totalRows)
+      }*/
+    }
+    else //data would be too long 
+    {
+    //document.getElementById("errors").innerHTML="This field can only have "+maxLines+" lines with "+charactersPerLine+" characters per line.";//display error message
+    //document.getElementById("count").innerHTML="";//remove digits count
+    cursorOffsetAfterOutput=-1;
+  }
+  document.querySelector(".codeSnip").value=textOutput;//1d: WRITE VALUE
+  document.querySelector(".codeSnip").selectionStart=cursorPositionOnInput+cursorOffsetAfterOutput; //2b: POSITION CURSOR
+  document.querySelector(".codeSnip").selectionEnd=cursorPositionOnInput+cursorOffsetAfterOutput; //set a single cursor, not a selection
+  onPaste=false;
+  //showOutput();
+  //toggleLabel();
+
+ 
+
+/*
+function showOutput(){
+    var outputHTMLtable="<table>";
+    var textInput=document.querySelector(".codeSnip").value;//READ DATA
+    var inputAsRows=textInput.split("\n");// create array from input => each element contains one row of the textarea
+    for(row=0;row<inputAsRows.length;row++)
+      {
+        outputHTMLtable=outputHTMLtable+"<tr><td>field "+row+":</td><td>"+inputAsRows[row]+"</td></tr>";
+      }
+    outputHTMLtable=outputHTMLtable+"</table>";
+    document.getElementById("output").innerHTML=outputHTMLtable;
+  }
+
+*/
+
+/*codeSnip.addEventListener('input', ()=>{
 
     let temp = calcLines();
     
 
-})
+});
+*/
+LinCount.textContent = totalRows;
+charCount.textContent = inputAsOneLine.length;
+console.log(totalRows);
+console.log(characterCount);
 
-   
-let codeSnipLineHeight = 20; // This should match the line-height in the CSS
-
-
-function calcLines(height, numlines){
-
-    let codeSnipHeight = codeSnip.scrollHeight; // Get the scroll height of the textarea
-    let numberOfLines = Math.floor(codeSnipHeight/codeSnipLineHeight);
-
-    let newLine = document.createElement('p');
-    newLine.className = 'codeLine';
-    newLine.textContent = '2';
-    side.appendChild(newLine);
-    codeBlock.style.height = `${codeSnipHeight}px`;
-
-    let codeHeight = codeSnipHeight/numberOfLines;
-
-    codeLine.style.height = `${codeHeight}px`;
+numOfRow = totalRows;
+createPElements();
+}
 
 
+function createPElements(){
+
+if (side.childElementCount < numOfRow)
+{
+ 
+  
+        let p = document.createElement('p');
+        p.className = `codeLine p${numOfRow+1}`;
+        p.textContent = `${numOfRow+1}`;
+        side.appendChild(p);
+        console.log("This is p[i]" + pElements[numOfRow]);
+        codeBlock.style.height = `${20+numOfRow*10}px`;
+    
+    console.log("p is this long"+side.childElementCount);
+}
+else if(side.childElementCount > numOfRow){
+  console.log("This is p[i]" + p[i]);
+  for (i=pElements.length; i > numOfRow; i--)
+    {
+      console.log("This is p[i]" + p[i]);
+        p[i].remove();
+    }
+}
+else{
+  return;
+}
 
 }
